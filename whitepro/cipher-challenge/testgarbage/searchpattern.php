08b6c9fea1bd9c17914127ab05626404c8ace741
Juan de los palotes <ezequiel.desimone@brightcove.com>
2017-06-01 17:29:08 -0300

    whitepro

--- whitepro/cipher-challenge/testgarbage/searchpattern.php
+++ whitepro/cipher-challenge/testgarbage/searchpattern.php
@@ -18,8 +18,67 @@ $plainFilePath = "./plain.txt";
 $plainFile = file($plainFilePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 
+function getCode ($str) {
+
+    $long = strlen($str);
+    $acharspecial = array(
+        " " => "S",
+        "." => "X",
+        "," => "Y",
+        ";" => "Z",
+        "[" => "L",
+        "]" => "R",
+        "-" => "M",
+        "#" => "H",
+        "*" => "A",
+        "!" => "E",
+        ":" => "D",
+        "?" => "Q",
+    );
+
+    $cont=0;
+    $result="";
+
+    for($i = 0; $i < $long; $i++) {
+        if (array_key_exists($str[$i], $acharspecial)) {
+            $result.= $cont;
+            $result.= $acharspecial[$str[$i]];
+            $cont=0;
+        } else {
+            $cont++;
+        }
+    }
+    if ( $result === "") {
+        $result=$cont;
+    }
+    return $result;
+
+}
+
+ini_set('max_execution_time', 300);
+
 $max = sizeof($encriptedFile);
-for($i = 0; $i < $max;$i++)
+$maxPlainFile = sizeof($plainFile);
+
+//$max = 1000;
+//$maxPlainFile = 1000;
+
+for($i = 0; $i < $max; $i++)
 {
     $trimmed = trim($encriptedFile[$i]);
+    //$lineOfEncript = (string) getCode($trimmed);
+    $lineOfEncript = getCode($trimmed);
+
+    for($j = 0; $j < $maxPlainFile; $j++)
+    {
+        $trimmedPlain = trim($plainFile[$j]);
+        //$lineOfPlain = (string) getCode($trimmedPlain);
+        $lineOfPlain = getCode($trimmedPlain);
+
+        If ($lineOfEncript === $lineOfPlain) {
+            echo "posible valor para crear table <br>";
+            echo "line: ".$i."-->".$encriptedFile[$i]."=== line: ".$j."-->".$plainFile[$j];
+            echo "<br>--------------<br>";
+        }
+    }
 }
