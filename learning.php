d5e48dafc5b55ba98f1e82b3454e1f8b5d7bb3ec
Juan de los palotes <ezequiel.desimone@brightcove.com>
2017-08-05 01:52:26 -0300

    revolve conflict with javapure

--- learning.php
+++ learning.php
@@ -7,20 +7,48 @@
  */
 
 
-$stdin = fopen('php://stdin', 'r');
+//$stdin = fopen('php://stdin', 'r');
+//
+//
+//$f = fopen( 'php://stdin', 'r' );
+//
+//
+//$tt= fgets( $f );
+//
+//
+//while( $line = fgets( $f ) ) {
+//    echo $line;
+//}
+//
+//fclose( $f );
+//
+//print_r($stdin);
+
+    $array = array(1, 2, 3, 9, 10, 11, 14);
 
 
-$f = fopen( 'php://stdin', 'r' );
+    function pasteTogether($val)
+    {
+        $newVals = array();
+        $min = $val[0];
+        $max = $val[1];
+        $counter = 0;
+        for ($i = 0; $i < count($val); $i++)
+        {
+            if ($val[$i + 1] === $val[$i] + 1)
+            {
+                $max = $val[$i + 1];
+            }
+            else
+            {
+                $tempVal = $min."-".$max;
+                $newVals[$counter] = $tempVal;
+                $counter++;
+                $min = $val[$i];
+            }
+        }
+        return $newVals;
+    }
 
+    pasteTogether($array);
 
-$tt= fgets( $f );
-
-print_r()
-while( $line = fgets( $f ) ) {
-    echo $line;
-}
-
-fclose( $f );
-
-print_r($stdin);
-?>
