5966feffff1f4d7e60e255ca80ece78e540ae84f
mariajosealvarez <maria.alvarez@brightcove.com>
2017-09-10 21:31:46 -0300

    add handler on panel

--- reactuto/my-app/src/List.jsx
+++ reactuto/my-app/src/List.jsx
@@ -8,25 +8,39 @@ class List extends Component  {
 	   	super(props);
 	   	this.state = {
 	   		years: [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
+	   		currentYear: 2005,
 	   	}
+
+	   	// This binding is necessary to make `this` work in the callback
+    	this.onSelectPanel = this.onSelectPanel.bind(this);
 	};
+
+	onSelectPanel(year, e){
+		e.preventDefault();
+		/*if (e.currentTarget.className === 'collapsed') {
+			this.setState({currentYear: year});
+		}*/
+	}
 
 	render() {
 	    return (
 				<div>
 					<h1>List of F1 World Champions</h1>
 					<div>
-					{
-						this.state.years.map(function(year){
-							const panel = year;
-		                        return <PanelGroup key={year}>
-									<Panel collapsible header={panel} eventKey="{year}">
-										<Season year={year}/>
+						<PanelGroup>
+						{
+							this.state.years.map(function(year){
+		                        return (
+									<Panel collapsible header={year} eventKey={year} key={year} onSelect={this.onSelectPanel} >
+										{
+											//year === this.state.currentYear &&
+												<Season year={year}/>
+										}
 									</Panel>
-								</PanelGroup>
-
-							})
-					}
+									)								
+								}, this)
+						}
+						</PanelGroup>
 					</div>
 				</div>
 			);
